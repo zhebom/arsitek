7452c8062e3e4a3a6fdfa1b2ad674bdac32f4a8a
zhebom <zhebom@gmail.com>
2023-07-23 16:49:13 +0700

    oke

--- app/Views/template/layout.php
+++ app/Views/template/layout.php
@@ -122,6 +122,30 @@
         <link rel="stylesheet" href="/adminlte/plugins/fontawesome-free/css/all.min.css">
         <!-- Theme style -->
         <link rel="stylesheet" href="/adminlte/dist/css/adminlte.min.css">
+
+        <!-- Tambahan -->
+        <!-- Google Font: Source Sans Pro -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+        <script src="https://kit.fontawesome.com/14004c86b9.js" crossorigin="anonymous"></script>
+        <!-- Ionicons -->
+        <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+        <!-- Tempusdominus Bootstrap 4 -->
+        <link rel="stylesheet" href="<?= base_url('/adminlte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css'); ?>">
+        <!-- iCheck -->
+        <link rel="stylesheet" href="<?= base_url('/adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css'); ?>">
+        <!-- JQVMap -->
+        <link rel="stylesheet" href="<?= base_url('/adminlte/plugins/jqvmap/jqvmap.min.css'); ?>">
+        <!-- Theme style -->
+        <link rel="stylesheet" href="<?= base_url('/adminlte/dist/css/adminlte.min.css'); ?>">
+        <!-- overlayScrollbars -->
+        <link rel="stylesheet" href="<?= base_url('/adminlte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css'); ?>">
+        <!-- Daterange picker -->
+        <link rel="stylesheet" href="<?= base_url('/adminlte/plugins/daterangepicker/daterangepicker.css'); ?>">
+        <!-- summernote -->
+        <link rel="stylesheet" href="<?= base_url('/adminlte/plugins/summernote/summernote-bs4.min.css'); ?>">
+        <!-- Ekko Lightbox -->
+        <link rel="stylesheet" href="<?= base_url('/plugins/ekko-lightbox/ekko-lightbox.css'); ?>">
 </head>
 
 <body>
@@ -171,11 +195,11 @@
                                         <ul class="dropdown-menu ">
                                             <li><a class="dropdown-item" href="#">Profile</a></li>
                                             <li><a class="dropdown-item" href="#">Riwayat Pelatihan</a></li>
-                                            
+
                                         </ul>
                                     </li>
                                     <li class="nav-item">
-                                    <a class="nav-link btn btn-danger" href="<?= base_url('/bom'); ?>"><strong>Logout</strong></a>
+                                        <a class="nav-link btn btn-danger" href="<?= base_url('/bom'); ?>"><strong>Logout</strong></a>
                                     </li>
                                 </ul>
                             </div>
@@ -237,6 +261,69 @@
     myModal.addEventListener('shown.bs.modal', function() {
         myInput.focus()
     })
+</script>
+<!-- jQuery -->
+<script src="<?= base_url('/adminlte/plugins/jquery/jquery.min.js'); ?>"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="<?= base_url('/adminlte/plugins/jquery-ui/jquery-ui.min.js'); ?>"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+    $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="<?= base_url('/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
+<!-- ChartJS -->
+<script src="<?= base_url('/adminlte/plugins/chart.js/Chart.min.js'); ?>"></script>
+<!-- Sparkline -->
+<script src="<?= base_url('/adminlte/plugins/sparklines/sparkline.js'); ?>"></script>
+<!-- JQVMap -->
+<script src="<?= base_url('/adminlte/plugins/jqvmap/jquery.vmap.min.js'); ?>"></script>
+<script src="<?= base_url('/adminlte/plugins/jqvmap/maps/jquery.vmap.usa.js'); ?>"></script>
+<!-- jQuery Knob Chart -->
+<script src="<?= base_url('/adminlte/plugins/jquery-knob/jquery.knob.min.js'); ?>"></script>
+<!-- daterangepicker -->
+<script src="<?= base_url('/adminlte/plugins/moment/moment.min.js'); ?>"></script>
+<script src="<?= base_url('/adminlte/plugins/daterangepicker/daterangepicker.js'); ?>"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="<?= base_url('/adminlte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js'); ?>"></script>
+<!-- Summernote -->
+<script src="<?= base_url('/adminlte/plugins/summernote/summernote-bs4.min.js'); ?>"></script>
+<!-- overlayScrollbars -->
+<script src="<?= base_url('/adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js'); ?>"></script>
+<!-- AdminLTE App -->
+<script src="<?= base_url('/adminlte/dist/js/adminlte.js'); ?>"></script>
+<!-- DataTables  & Plugins -->
+<script src="<?= base_url('/adminlte/plugins/datatables/jquery.dataTables.min.js'); ?>"></script>
+<script src="<?= base_url('/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js'); ?>"></script>
+<script src="<?= base_url('/adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js'); ?>"></script>
+<script src="<?= base_url('/adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js'); ?>"></script>
+<script src="<?= base_url('/adminlte/plugins/datatables-buttons/js/dataTables.buttons.min.js'); ?>"></script>
+<script src="<?= base_url('/adminlte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js'); ?>"></script>
+<script src="<?= base_url('/adminlte/plugins/jszip/jszip.min.js'); ?>"></script>
+<script src="<?= base_url('/adminlte/plugins/pdfmake/pdfmake.min.js'); ?>"></script>
+<script src="<?= base_url('/adminlte/plugins/pdfmake/vfs_fonts.js'); ?>"></script>
+<script src="<?= base_url('/adminlte/plugins/datatables-buttons/js/buttons.html5.min.js'); ?>"></script>
+<script src="<?= base_url('/adminlte/plugins/datatables-buttons/js/buttons.print.min.js'); ?>"></script>
+<script src="<?= base_url('/adminlte/plugins/datatables-buttons/js/buttons.colVis.min.js'); ?>"></script>
+<!-- Page specific script -->
+<script>
+    $(function() {
+        $("#example1").DataTable({
+            "responsive": true,
+            "lengthChange": false,
+            "autoWidth": false,
+            "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+        $('#example2').DataTable({
+            "paging": true,
+            "lengthChange": false,
+            "searching": false,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "responsive": true,
+        });
+    });
 </script>
 
 </html>
