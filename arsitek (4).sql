64d6fa7558eafa32d5c828a3c77e947323821b38
zhebom <zhebom@gmail.com>
2023-07-25 23:51:03 +0700

    oke

--- arsitek (4).sql
+++ arsitek (4).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 22, 2023 at 09:42 AM
+-- Generation Time: Jul 25, 2023 at 06:50 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -90,10 +90,9 @@ CREATE TABLE `pelatihan` (
 --
 
 INSERT INTO `pelatihan` (`id`, `pelatihan`, `slug`, `kuota`, `biaya`, `tempat`, `gambar`, `tglpelatihan`, `endpendaftaran`, `updated_at`, `deleted_at`) VALUES
-(1, 'Autocad September 2023 Batch 3', 'Autocad-September-2023-Batch-3', 25, 0, '', 'Alny (2).png', '2023-07-10', '2023-07-08', '2023-07-02 07:35:38', '0000-00-00 00:00:00'),
-(2, 'Autocad September 2023 Batch 4', 'Autocad-September-2023-Batch-4', 100, 0, '', 'Pembagian Kelompok SKPI.png', '2023-07-23', '2023-07-20', '2023-07-07 17:16:40', '0000-00-00 00:00:00'),
-(4, 'Sketch Up Training', 'Sketch-Up-Training', 25, 0, 'Trasa Co-Working Space', 'coworking space_1.jpeg', '2023-07-23', '2023-07-22', '2023-07-10 07:17:57', '0000-00-00 00:00:00'),
-(5, 'Sketch Up Training 2', 'Sketch-Up-Training-2', 100, 50000, 'Trasa Co-Working Space', 'RPL Akuntnasi & Akuntansi_1.png', '2023-07-27', '2023-07-24', '2023-07-12 10:01:03', '0000-00-00 00:00:00');
+(2, 'Autocad September 2023 Batch 4', 'Autocad-September-2023-Batch-4', 100, 25000, '', 'Pembagian Kelompok SKPI.png', '2023-07-23', '2023-07-20', '2023-07-23 15:54:12', '0000-00-00 00:00:00'),
+(4, 'Sketch Up Training', 'Sketch-Up-Training', 25, 25000, 'Trasa Co-Working Space', 'coworking space_1.jpeg', '2023-07-23', '2023-07-22', '2023-07-23 15:54:21', '0000-00-00 00:00:00'),
+(5, 'Sketch Up Training 2', 'Sketch-Up-Training-2', 10, 50000, 'Trasa Co-Working Space', 'download (1).png', '2023-07-27', '2023-07-24', '2023-07-25 08:17:23', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -108,6 +107,7 @@ CREATE TABLE `shopping_cart` (
   `biaya_pelatihan` int(255) NOT NULL,
   `total_pesanan` int(255) NOT NULL,
   `kode_pesanan` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -116,10 +116,15 @@ CREATE TABLE `shopping_cart` (
 -- Dumping data for table `shopping_cart`
 --
 
-INSERT INTO `shopping_cart` (`id`, `id_user`, `id_pelatihan`, `biaya_pelatihan`, `total_pesanan`, `kode_pesanan`, `created_at`, `updated_at`) VALUES
-(1, 0, 1, 50000, 1, '1689771261', '0000-00-00 00:00:00', '2023-07-19 12:54:21'),
-(2, 1, 5, 50000, 1, '1689771863', '0000-00-00 00:00:00', '2023-07-19 13:04:23'),
-(3, 2, 5, 50000, 1, '1690010482', '0000-00-00 00:00:00', '2023-07-22 07:21:22');
+INSERT INTO `shopping_cart` (`id`, `id_user`, `id_pelatihan`, `biaya_pelatihan`, `total_pesanan`, `kode_pesanan`, `token`, `created_at`, `updated_at`) VALUES
+(1, 0, 1, 50000, 1, '1689771261', '', '0000-00-00 00:00:00', '2023-07-19 12:54:21'),
+(2, 1, 5, 50000, 1, '1689771863', '', '0000-00-00 00:00:00', '2023-07-19 13:04:23'),
+(3, 2, 5, 50000, 1, '1690010482', '', '0000-00-00 00:00:00', '2023-07-22 07:21:22'),
+(4, 2, 1, 0, 1, '1690127614', '', '0000-00-00 00:00:00', '2023-07-23 15:53:34'),
+(5, 2, 2, 25000, 1, '1690127695', '', '0000-00-00 00:00:00', '2023-07-23 15:54:55'),
+(6, 2, 5, 50000, 1, '1690129491', '8dfeb679-44c1-4320-938b-6f277017ed7c', '0000-00-00 00:00:00', '2023-07-23 16:24:51'),
+(7, 2, 5, 50000, 1, '1690129664', 'fa652b7d-12a4-4a50-800e-55210681fb8d', '0000-00-00 00:00:00', '2023-07-23 16:27:44'),
+(8, 2, 5, 50000, 2, '1690131570', '005b9586-141c-4253-8010-f388a98a0163', '0000-00-00 00:00:00', '2023-07-23 16:59:31');
 
 -- --------------------------------------------------------
 
@@ -181,7 +186,7 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `user`, `pass`, `nama`, `no_ktp`, `role`, `created_at`, `updated_at`) VALUES
 (1, 'admin@ruangteksi.com', 'cc6b6fc05e689a3e5000269f8712e1c9', 'Admin Punya', '', 2, '2023-07-22', '0000-00-00 00:00:00'),
-(2, 'admin2@ruangteksi.com', '$2y$10$MkBqljL9BLBkhf9t14cure8oWUW3o5tS39PCWc858mUImcUc3kms6', 'Widhi', '', 2, '2023-07-22', '0000-00-00 00:00:00');
+(2, 'admin2@ruangteksi.com', '$2y$10$6Pp6oQ6Rr1WDOxmSUstH6ey3LR/16FZgecMUWQYVTby1.w6FXFQFi', 'Widhiawan Agung K', '082136505440', 2, '2023-07-22', '2023-07-24 13:41:32');
 
 --
 -- Indexes for dumped tables
@@ -249,13 +254,13 @@ ALTER TABLE `faq`
 -- AUTO_INCREMENT for table `pelatihan`
 --
 ALTER TABLE `pelatihan`
-  MODIFY `id` bigint(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `shopping_cart`
 --
 ALTER TABLE `shopping_cart`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `sosmed`
